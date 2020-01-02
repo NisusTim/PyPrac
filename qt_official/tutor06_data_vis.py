b967945fa72b89145c45d9c8113c98392d66e04e
NisusTim <nisustim@gmail.com>
2020-01-02 10:38:31 +0800

    NsTm/qt_official: tutor06_data_vis
      1. pandas: data filtering, timezone

--- qt_official/tutor06_data_vis.py
+++ qt_official/tutor06_data_vis.py
@@ -1,9 +1,22 @@
 # python3 tutor06_data_vis.py -f <*.csv>
 import argparse  # accept and parse input from CLI
 import pandas as pd
+from PyQt5.QtCore import QDateTime, QTimeZone
+
+def transform_date(utc, timezone=None):
+  utc_fmt = 'yyyy-MM-ddTHH:mm:ss.zzzZ'
+  new_date = QDateTime().fromString(utc, utc_fmt)
+  if timezone:
+    new_date.setTimeZone(timezone)
+  return new_date
 
 def read_data(fname):
-  return pd.read_csv(fname)
+  df = pd.read_csv(fname)
+  df = df.drop(df[df['mag'] < 0].index)  # remove wrong magnitudes
+  magnitudes = df['mag']
+  timezone = QTimeZone(b"Europe/Berlin")
+  times = df['time'].apply(lambda x: transform_date(x, timezone))
+  return times, magnitudes
 
 if __name__ == '__main__':
   options = argparse.ArgumentParser()
