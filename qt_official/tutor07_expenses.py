53be5c9b53ce8aa217a0e3c111c5cd0be4d86cbf
NisusTim <nisustim@gmail.com>
2020-01-10 12:04:37 +0800

    NsTm/qt_official: tutor07_expenses
      1. connect buttons with event function via signals and slots

--- qt_official/tutor07_expenses.py
+++ qt_official/tutor07_expenses.py
@@ -8,7 +8,7 @@ DATA = {"Water": 24.5, "Electricity": 55.1, "Rent": 850.0,
 
 if __name__ == '__main__':
   app = QApplication([])
-  widget = Widget(DATA)
-  window = MainWindow(app, widget)
+  widget = Widget(DATA, app.quit)
+  window = MainWindow(widget, app.quit)
   window.show()
   app.exec_()
--- qt_official/tutor07_expenses_window.py
+++ qt_official/tutor07_expenses_window.py
@@ -3,23 +3,23 @@ from PySide2.QtCore import Slot
 
 class MainWindow(QMainWindow):
 
-  def __init__(self, app, widget):
+  def __init__(self, widget, app_quit):
     QMainWindow.__init__(self)
-    self.setWindowTitle("Expenses Tool")
+    self.kAppQuit = app_quit
     self.setCentralWidget(widget)
-    self.app = app
+    self.setWindowTitle("Expenses Tool")
     # Menu
     self.menu = self.menuBar()
     self.file_menu = self.menu.addMenu("File")
     exit_action = QAction("Exit", self)
     exit_action.setShortcut("Ctrl+Q")
-    # exit_action.triggered.connect(self.close)
-    exit_action.triggered.connect(self.Exit)
+    # exit_action.triggered.connect(self.close)  # connect QMaindWindow.close()
+    exit_action.triggered.connect(self.Exit)  # connect QApplication.quit()
     self.file_menu.addAction(exit_action)
     # Window dimensions
     geometry = (800, 600)
     self.resize(*geometry)
 
-  @Slot(bool)
-  def Exit(self, checked):
-    self.app.quit()
+  @Slot()
+  def Exit(self):
+    self.kAppQuit()
