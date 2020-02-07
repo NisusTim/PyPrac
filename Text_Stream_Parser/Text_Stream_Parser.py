2357aa2bb25d42809dbc901896784b90b41157a1
NisusTim <nisustim@gmail.com>
2020-02-07 18:46:48 +0800

    TmCh/develop: parsers

--- Text_Stream_Parser/Text_Stream_Parser.py
+++ Text_Stream_Parser/Text_Stream_Parser.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import re
+import string
 
 class TextStreamParser:
   """ text stream parser by regex """
@@ -8,6 +9,7 @@ class TextStreamParser:
   Pattern = namedtuple('Pattern', ('format', 'field'))
 
   _RE_NUM = "([-]?[0-9]+[.]?[0-9]*)"
+  _RE_HEX = "((0[xX])?[0-9a-fA-F]+)"
   _RE_INT = "([-]?[0-9]+)"
   _RE_FLT = "([-]?[0-9]+[.][0-9]+)"
 
@@ -45,7 +47,51 @@ class FrameParser:
   def __init__(self):
     pass
 
+class TypeCast:
+  @staticmethod
+  def str_tuple_to_num(item, base=10):
+    """str elements in list/tuple cast to num elements in tuple"""
+
+    if any(isinstance(e, list) for e in item) \
+      or any(isinstance(e, tuple) for e in item) and len(item) == 1:
+      return tuple(TypeCast.str2num(e, base) for ls in item for e in ls)
+    else:
+      return tuple(tuple(TypeCast.str2num(e, base) for e in ls) for ls in item)
+
+  @staticmethod
+  def str2num(s, base=10):
+    """cast str to int or float"""
+
+    if not isinstance(s, str) or not s:
+      return None
+    elif s.isdigit():
+      return int(s, base)
+    elif all(c in set(string.hexdigits) for c in s):
+      return int(s, 16)
+    elif s.find('.') != -1:
+      try:
+        float(s)
+        return float(s)
+      except:
+        return s
+
+import unittest
+class TypeCastTestCase(unittest.TestCase):
+  def test_str_tuple_to_num(self):
+    input1 = (('450', '7', '38.3', '0.19990'), ('150', '182666', '26.3', '0.29111'))
+    expect1 = ((450, 7, 38.3, 0.19990), (150, 182666, 26.3, 0.29111))
+    self.assertEqual(TypeCast.str_tuple_to_num(input1), expect1)
+    self.assertEqual(TypeCast.str_tuple_to_num((input1[0], )), expect1[0])
+
+  def test_str2num(self):
+    self.assertEqual(TypeCast.str2num("3.14"), 3.14)
+    self.assertEqual(TypeCast.str2num("9487"), 9487)
+    self.assertEqual(TypeCast.str2num("ABCD"), 0xABCD)
+    self.assertEqual(TypeCast.str2num("0xABCD"), None)
+
 if __name__ == '__main__':
-  t = TextStreamParser(1)
-  f = FrameParser()
-  print(f.b)
+  # unittest.main()
+  # t = TextStreamParser(1)
+  # f = FrameParser()
+  # print(f.b)
+  pass
